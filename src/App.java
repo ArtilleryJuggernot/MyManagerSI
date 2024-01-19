947bc0780a60ffde7483bd25b94f423c17bb0a0b
ArtilleryJuggernot <artillery@artillery.fr>
2024-01-19 15:57:24 +0100

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+// MMSI
+
+import Login.ControllerLogin;
+import Login.LoginPage;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		ControllerLogin c = new ControllerLogin(new LoginPage());
+	}
+
+}
+
+
---
+++ src/Login/ControllerLogin.java
@@ -0,0 +1,17 @@
+package Login;
+
+public class ControllerLogin {
+private LoginPage f;
+	
+	public ControllerLogin(LoginPage f) {
+		super();
+		this.f = f;
+		init();
+		f.setVisible(true);
+	}
+	
+	
+	private void init() {
+		
+	}
+}
